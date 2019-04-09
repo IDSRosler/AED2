b766b48aae0f3f86b9d4acc1e1ff1a8cabd832a5
IDSRosler <igorsouzarosler@gmail.com>
2019-04-08 23:17:13 -0300

    Código pronto

--- Grafo/main.c
+++ Grafo/main.c
@@ -18,63 +18,71 @@ typedef struct grafo {
 } Grafo;
 
 Grafo* cria_grafo(int num_vertices, int grau_max, int eh_ponderado);
-void inserir_aresta(Grafo* gr, int orig, int dest, int eh_digrafo, float peso);
+int inserir_aresta(Grafo* gr, int orig, int dest, int eh_digrafo, float peso);
 void liberar_grafo(Grafo* gr);
 int procura_menor_distancia(float* dist, int* visitado, int nv);
 void menor_caminho(Grafo* gr, int ini, int* ant, float* dist);
 
 int main() {
     int num_vertices, grau_max, eh_ponderado, inserir, orig, dest, eh_digrafo;
-    int v_origem, v_destino, *v_anterior;
-    float peso = 0, *distancia;
+    int v_origem, v_destino, aux;
+    float peso = 1.0;
     Grafo *gr;
 
-    /*printf("Digite a quantidade de vertices do grafo:  ");
+    printf("Digite a quantidade de vertices do grafo:  ");
     scanf("%d", &num_vertices);
-    printf("\nDigite a quantidade maxima de arestas que os vertices podem ter:  ");
+    printf("Digite a quantidade maxima de arestas que os vertices podem ter:  ");
     scanf("%d", &grau_max);
-    printf("\nO grafo e ponderado?(1 - SIM  |  0 - NAO):  ");
+    printf("O grafo e ponderado?(1 - SIM  |  0 - NAO):  ");
     scanf("%d", &eh_ponderado);
 
     gr = cria_grafo(num_vertices, grau_max, eh_ponderado);
 
-    printf("\nInserir aresta(1 - SIM  |  0 - NAO):  ");
+    printf("Inserir aresta(1 - SIM  |  0 - NAO):  ");
     scanf("%d", &inserir);
     inserir = (inserir != 1) ? 0 : 1; // Qualquer escolha diferente de 1 será considerado como alternativa 'NÃO'
     do {
         if (inserir) {
-            printf("\nInforme o vertice origem e o vertice de destino da aresta:  ");
+            printf("Informe o vertice origem e o vertice de destino da aresta:  ");
             scanf("%d %d", &orig, &dest);
-            printf("\nO grafo e digrafo?(1 - SIM  |  0 - NAO):  ");
+            printf("O grafo e digrafo?(1 - SIM  |  0 - NAO):  ");
             scanf("%d", &eh_digrafo);
             eh_digrafo = (eh_digrafo != 1) ? 0 : 1; // Qualquer escolha diferente de 1 será considerado como alternativa 'NÃO'
             if (eh_ponderado) {
-                printf("\nInforme o peso da aresta: ");
+                printf("Informe o peso da aresta: ");
                 scanf("%f", &peso);
             }
             inserir_aresta(gr, orig, dest, eh_digrafo, peso);
-            printf("\nInserir outra aresta(1 - SIM  |  0 - NAO):  ");
+            printf("Inserir outra aresta(1 - SIM  |  0 - NAO):  ");
             scanf("%d", &inserir);
             inserir = (inserir != 1) ? 0 : 1; // Qualquer escolha diferente de 1 será considerado como alternativa 'NÃO'
         }
-    } while (inserir);*/
-    
-    gr = cria_grafo(4,3,1);
-    inserir_aresta(gr,0,1,1,3);
-    inserir_aresta(gr,1,3,1,1);
-    inserir_aresta(gr,1,2,1,2);
-    inserir_aresta(gr,2,0,1,2);
-    inserir_aresta(gr,2,3,1,5);
-    
+    } while (inserir);
+
+    /*gr = cria_grafo(4, 3, 1);
+    inserir_aresta(gr, 0, 1, 1, 3);
+    inserir_aresta(gr, 1, 3, 1, 5);
+    inserir_aresta(gr, 1, 2, 1, 2);
+    inserir_aresta(gr, 2, 0, 1, 2);
+    inserir_aresta(gr, 2, 3, 1, 1);*/
+
     printf("\nInforme o vertice origem e o vertice destino para o calculo do menor caminho: ");
     scanf("%d %d", &v_origem, &v_destino);
 
-    v_anterior = (int *) malloc(gr->num_vertices * sizeof (int));
-    distancia = (float *) malloc(gr->num_vertices * sizeof (float));
+    int v_anterior[gr->num_vertices];
+    float distancia[gr->num_vertices];
 
     menor_caminho(gr, v_origem, v_anterior, distancia);
 
-    printf("\n\nA menor distancia do vertice[%d] ate o vertice[%d] e: %.2f", v_origem, v_destino, distancia[v_destino]);
+    printf("\nA menor distancia do vertice[%d] ate o vertice[%d] e: %.f", v_origem, v_destino, distancia[v_destino]);
+    printf("\nCaminho: ");
+    aux = v_anterior[v_destino];
+    while (aux != -1) {
+        printf("Vertice[%d]\t", aux);
+        aux = v_anterior[aux];
+    }
+
+    liberar_grafo(gr);
 
     return 0;
 }
@@ -87,49 +95,47 @@ Grafo* cria_grafo(int num_vertices, int grau_max, int eh_ponderado) {
         gr->grau_max = grau_max;
         gr->eh_ponderado = (eh_ponderado != 0) ? 1 : 0; // Estrutura simplificada para if(eh_ponderado != 0) então retorna 1 caso contrario 0
         gr->grau = (int *) calloc(num_vertices, sizeof (int));
-        gr->arestas = (int **) malloc(num_vertices * sizeof (int));
+        gr->arestas = (int **) malloc(num_vertices * sizeof (int *));
         for (i = 0; i < num_vertices; i++) {
             gr->arestas[i] = (int *) malloc(grau_max * sizeof (int));
         }
-        if (eh_ponderado) {
-            gr->pesos = (float **) malloc(num_vertices * sizeof (float));
-            for (i = 0; i < num_vertices; i++) {
-                gr->pesos[i] = (float *) malloc(grau_max * sizeof (float));
-            }
+        gr->pesos = (float **) malloc(num_vertices * sizeof (float *));
+        for (i = 0; i < num_vertices; i++) {
+            gr->pesos[i] = (float *) malloc(grau_max * sizeof (float));
         }
     }
     return gr;
 }
 
 void liberar_grafo(Grafo* gr) {
-    if (gr == NULL) return;
-    int i;
-    for (i = 0; i < gr->num_vertices; i++) {
-        free(gr->arestas[i]);
-    }
-    free(gr->arestas);
-    if (gr->eh_ponderado) {
+    if (gr != NULL) {
+        int i;
         for (i = 0; i < gr->num_vertices; i++) {
-            free(gr->pesos[i]);
+            free(gr->arestas[i]);
         }
-        free(gr->pesos);
+        free(gr->arestas);
+        if (gr->eh_ponderado) {
+            for (i = 0; i < gr->num_vertices; i++) {
+                free(gr->pesos[i]);
+            }
+            free(gr->pesos);
+        }
+        free(gr->grau);
+        free(gr);
     }
-    free(gr->grau);
-    free(gr);
 }
 
-void inserir_aresta(Grafo* gr, int orig, int dest, int eh_digrafo, float peso) {
-    if (gr == NULL) return;
-    if (orig < 0 || orig >= gr->num_vertices) return;
-    if (dest < 0 || dest >= gr->num_vertices) return;
+int inserir_aresta(Grafo* gr, int orig, int dest, int eh_digrafo, float peso) {
+    if (gr == NULL) return 0;
+    if (orig < 0 || orig >= gr->num_vertices) return 0;
+    if (dest < 0 || dest >= gr->num_vertices) return 0;
     gr->arestas[orig][gr->grau[orig]] = dest;
-    if (gr->eh_ponderado) {
-        gr->pesos[orig][gr->grau[orig]] = peso;
-    }
+    gr->pesos[orig][gr->grau[orig]] = peso;
     gr->grau[orig]++;
     if (eh_digrafo == 0) { // 1 - Indica que é dígrafo (direcional) / 0 - Indica que não é digrafo (não direcinado) 
         inserir_aresta(gr, dest, orig, 1, peso);
     }
+    return 1;
 }
 
 void menor_caminho(Grafo* gr, int ini, int* ant, float* dist) {
@@ -139,7 +145,7 @@ void menor_caminho(Grafo* gr, int ini, int* ant, float* dist) {
     for (i = 0; i < nv; i++) { // Inicialização
         ant[i] = -1;
         dist[i] = -1;
-        visitado = 0;
+        visitado[i] = 0;
     }
     dist[ini] = 0;
     while (cont > 0) {
@@ -156,7 +162,7 @@ void menor_caminho(Grafo* gr, int ini, int* ant, float* dist) {
             } else {
                 if (dist[ind] > dist[u] + gr->pesos[u][i]) {
                     dist[ind] = dist[u] + gr->pesos[u][i];
-                    dist[ind] = u;
+                    ant[ind] = u;
                 }
             }
         }
